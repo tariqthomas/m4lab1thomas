49725a7f9b847bc6df6cccd2d0602deaf0f8a7c9
tariqthomas <thomast0609@student.faytechcc.edu>
2025-04-06 22:49:48 +0000

    m4lab1thomas.cpp

---
+++ m4lab1thomas.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+
+int hieght, width;
+
+hieght=5;
+width=5;
+
+for (int j=0; j < hieght; j++) {
+    for (int i=0; i < width; i++) {
+  cout <<"*" << "";
+    }
+ cout << endl;
+}
+
+
+
+}
